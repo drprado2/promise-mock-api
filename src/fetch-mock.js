a7f2ce1326f66956f7b7dfcf12f4c24264318e06
adriano.oliveira <adriano.oliveira@hbsis.com.br>
2018-04-05 16:33:39 -0300

    Refatorado fetch mock para usar closures e encapsular propriedades.

--- src/fetch-mock.js
+++ src/fetch-mock.js
@@ -1,50 +1,61 @@
-export default class FetchMock{
+const FetchMock = (function(){
 
-  static _apis = [];
+  let _apis = [];
 
-  static loadApis(...apis){
-    FetchMock._apis =  FetchMock._apis.concat(apis);
-  }
+  const _findApi = url =>!_apis ? null : _apis.find(x => url.match(x.baseUrl));
 
-  static _findApi = url => {
-    if(!FetchMock._apis)
-      return null;
+  const _getResource = (url, baseUrl) => url.substring(baseUrl.length);
 
-    return FetchMock._apis.find(x => url.match(x.baseUrl));
-  }
+  const loadApis = (...apis) => _apis =  _apis.concat(apis);
 
-  static _getResource = (url, baseUrl) => url.substring(baseUrl.length);
+  const get = (url, queryString, forcedResult=null) => {
+    const api = _findApi(url);
 
-  static get(url, queryString, forcedResult=null){
-    const api = FetchMock._findApi(url);
     if(!api)
       return Promise.reject(new Response(null, {"status": 404}));
-    const resource = FetchMock._getResource(url, api.baseUrl);
+
+    const resource = _getResource(url, api.baseUrl);
     return api.get(resource, queryString, forcedResult);
   }
 
-  static post(url, bodyObj, forcedResult=null){
-    const api = FetchMock._findApi(url);
+  const post = (url, bodyObj, forcedResult=null) => {
+    const api = _findApi(url);
+
     if(!api)
       return Promise.reject(new Response(null, {"status": 404}));
-    const resource = FetchMock._getResource(url, api.baseUrl);
+
+    const resource = _getResource(url, api.baseUrl);
     return api.post(resource, bodyObj, forcedResult);
   }
 
-  static put(url, id, bodyObj, forcedResult=null){
-    const api = FetchMock._findApi(url);
+  const put = (url, id, bodyObj, forcedResult=null) => {
+    const api = _findApi(url);
+
     if(!api)
       return Promise.reject(new Response(null, {"status": 404}));
-    const resource = FetchMock._getResource(url, api.baseUrl);
+
+    const resource = _getResource(url, api.baseUrl);
     return api.put(resource, id, bodyObj, forcedResult);
   }
 
-  static delete(url, id, forcedResult=null){
-    const api = FetchMock._findApi(url);
+  const del = (url, id, forcedResult=null) => {
+    const api = _findApi(url);
+
     if(!api)
       return Promise.reject(new Response(null, {"status": 404}));
-    const resource = FetchMock._getResource(url, api.baseUrl);
+
+    const resource = _getResource(url, api.baseUrl);
     return api.delete(resource, id, forcedResult);
   }
-}
+
+  return {
+    loadApis,
+    get,
+    post,
+    put,
+    delete: del
+  }
+})();
+
+export default FetchMock;
 
